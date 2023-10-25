f5a3c5ec93d9e517470a2206af13225dce2334cc
DotPack <lifirenko123@gmail.com>
2023-10-25 21:08:04 +0300

    add command start help

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-import aiogram
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-print('Hello, world!')
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="###########")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello! Я перший бот на Python! Більше інформації про мене тут /help")
+
+
+@dp.message(Command("help"))
+async def cmd_help(message: types.Message):
+    await message.answer("Чого ти хочеш? я просто бот, який нічого не робить")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
